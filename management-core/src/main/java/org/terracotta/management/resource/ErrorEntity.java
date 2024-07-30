d4d8e3c6cc8e252cc4efc91a503bfbec55bdf495
Nishchay <nishchay.naresh@gmail.com>
2024-07-30 13:17:37 +0530

    updating copyright header

--- management-core/src/main/java/org/terracotta/management/resource/ErrorEntity.java
+++ management-core/src/main/java/org/terracotta/management/resource/ErrorEntity.java
@@ -1,5 +1,6 @@
 /*
- * Copyright Terracotta, Inc.
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  * Copyright Super iPaaS Integration LLC, an IBM Company 2024
  */
 
